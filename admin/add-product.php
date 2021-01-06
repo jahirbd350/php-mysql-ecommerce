2fbd5f8715e0e605f854b7e618efdead1e6392df
jahirbd350 <jahirbd350@gmail.com>
2021-01-06 23:16:51 +0600

    06 Dec 2021

--- admin/add-product.php
+++ admin/add-product.php
@@ -10,9 +10,9 @@ $category = new Category();
 $allCategory = $category->allActiveCategory();
 
 if (isset($_POST['add_product'])){
-    /*echo '<pre>';
+    echo '<pre>';
     print_r($_POST);
-    echo '</pre>';*/
+    echo '</pre>';
 }
 
 ?>
@@ -54,63 +54,63 @@ if (isset($_POST['add_product'])){
                                 <div class="form-group row mb-4">
                                     <label for="product_code" class="col-sm-3 col-form-label">Product Code</label>
                                     <div class="col-sm-9">
-                                        <input type="text" id="product_code" name="product_code" class="form-control"  placeholder="Sub Category Description"/>
+                                        <input type="text" id="product_code" name="product_code" class="form-control"  placeholder="Product Code will be generated automatically" value=""/>
                                         <span></span>
                                     </div>
                                 </div>
                                 <div class="form-group row mb-4">
                                     <label for="product_brand" class="col-sm-3 col-form-label">Product Brand</label>
                                     <div class="col-sm-9">
-                                        <input type="text" id="product_brand" name="product_brand" class="form-control"  placeholder="Sub Category Description"/>
+                                        <input type="text" id="product_brand" name="product_brand" class="form-control"  placeholder="Enter Product brand"/>
                                         <span></span>
                                     </div>
                                 </div>
                                 <div class="form-group row mb-4">
                                     <label for="product_model" class="col-sm-3 col-form-label">Product Model</label>
                                     <div class="col-sm-9">
-                                        <input type="text" name="product_model" class="form-control" id="product_model"/>
+                                        <input type="text" name="product_model" class="form-control" id="product_model" placeholder="Enter Product Model"/>
                                         <span></span>
                                     </div>
                                 </div>
                                 <div class="form-group row mb-4">
                                     <label for="product_name" class="col-sm-3 col-form-label">Product Name</label>
                                     <div class="col-sm-9">
-                                        <input type="text" name="product_name" class="form-control" id="product_name"/>
+                                        <input type="text" name="product_name" class="form-control" id="product_name" placeholder="Enter Product Name"/>
                                         <span></span>
                                     </div>
                                 </div>
                                 <div class="form-group row mb-4">
-                                    <label for="product_size" class="col-sm-3 col-form-label">Product Size</label>
+                                    <label for="product_size" class="col-sm-3 col-form-label">Product Size/Unit</label>
                                     <div class="col-sm-9">
-                                        <input type="text" name="product_size" class="form-control" id="product_size"/>
+                                        <input type="text" name="product_size" class="form-control" id="product_size" placeholder="Enter Product Size or Unit"/>
                                         <span></span>
                                     </div>
                                 </div>
                                 <div class="form-group row mb-4">
                                     <label for="product_color" class="col-sm-3 col-form-label">Product Color</label>
                                     <div class="col-sm-9">
-                                        <input type="text" name="product_color" class="form-control" id="product_color"/>
+                                        <input type="text" name="product_color" class="form-control" id="product_color" placeholder="Enter Product Color"/>
                                         <span></span>
                                     </div>
                                 </div>
                                 <div class="form-group row mb-4">
                                     <label for="product_price" class="col-sm-3 col-form-label">Product Price</label>
                                     <div class="col-sm-9">
-                                        <input type="text" name="product_price" class="form-control" id="product_price"/>
+                                        <input type="text" name="product_price" class="form-control" id="product_price" placeholder="Enter Product price"/>
                                         <span></span>
                                     </div>
                                 </div>
                                 <div class="form-group row mb-4">
                                     <label for="product_description" class="col-sm-3 col-form-label">Product Description</label>
                                     <div class="col-sm-9">
-                                        <input type="text" name="product_description" class="form-control" id="product_description"/>
+                                        <input type="text" name="product_description" class="form-control" id="product_description" placeholder="Enter Product Description"/>
                                         <span></span>
                                     </div>
                                 </div>
                                 <div class="form-group row mb-4">
                                     <label for="product_image" class="col-sm-3 col-form-label">Product Images</label>
                                     <div class="col-sm-9">
-                                        <input type="file" name="product_image" class="form-control-file" id="product_image"/>
+                                        <input type="file" name="product_image" class="form-control-file" id="product_image" placeholder="Enter Product Images"/>
                                         <span></span>
                                     </div>
                                 </div>
@@ -148,6 +148,11 @@ include 'footer_admin.php';
                     }else{
                         $('#sub_category_name').html('<option value="">Select Category First</option>');
                     }
+                    $("#sub_category_name").on("change",function() {
+                        var subCategoryId = $(this).val();
+                        const randomNum = Math.floor(1000 + Math.random() * 9000);
+                        $("#product_code").val(categoryId + '-' + subCategoryId + '-' +randomNum);
+                    });
                 });
             });
         </script>
